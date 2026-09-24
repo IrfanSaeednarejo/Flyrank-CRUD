a4fdf1aff2e3b118fa24d7df20c7e89baa6ce9b3
Irfan Saeed <irfansaeed.narejo@gmail.com>
2026-09-25 00:16:22 +0500

    feat: A3 Containerize your stack

--- Connecting to the database/src/db/index.ts
+++ Connecting to the database/src/db/index.ts
@@ -3,7 +3,10 @@ import { Pool } from 'pg';
 import * as schema from './schema/tasks.ts';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString:
+        process.env.NODE_ENV === 'production'
+            ? process.env.DATABASE_URL_POOLED ?? process.env.DATABASE_URL
+            : process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
